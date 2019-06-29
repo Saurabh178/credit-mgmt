92e07a54f47526b1e297d520148d208ec65bec0c
Sauarbh Gupta <gupta.saurabh178@gmail.com>
2019-06-29 19:45:55 +0530

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,10 +1,6 @@
 <?php
 
-$url = parse_url(getenv('DATABASE_URL'));
-$host = $url['host']?? null;
-$username = $url['user'];
-$password = $url['pass'];
-$database = substr($url['path'], 1);
+use Illuminate\Support\Str;
 
 return [
 
