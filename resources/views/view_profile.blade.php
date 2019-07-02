5b57ab564c8adb696b013dbf463e9f10a1e621f9
Sauarbh Gupta <gupta.saurabh178@gmail.com>
2019-07-02 23:24:49 +0530

    Update view_profile.blade.php

--- resources/views/view_profile.blade.php
+++ resources/views/view_profile.blade.php
@@ -42,13 +42,6 @@
                                 <button type="button" class="close" data-dismiss="modal">&times;</button>
                             </div>
 
-                            @if(count($errors) > 0)
-                                <div class="alert alert-danger">
-                                    @foreach($errors->all() as $error)
-                                        <p>{{$error}}</p>
-                                    @endforeach
-                                </div>
-                            @endif
                             <div class="modal-body">
                                 <div>
                                     @include('modal_form')
